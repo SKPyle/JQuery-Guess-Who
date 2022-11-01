4716f9b224be5b90418134bb22d37284345ad47d
Seth Pyle <sethpyle@yahoo.com>
2022-10-31 22:15:48 -0500

    created render people funciton, looped through to append userprofile. randomized names with randomizer function

--- client.js
+++ client.js
@@ -1,1 +1,43 @@
-console.log('Here are all the available people:', people);
+$(document).ready(onReady);
+
+
+
+console.log("client.js has been loaded");
+
+function onReady() {
+  console.log("jquery is ready!");
+  renderPeople();
+  nameRandomizor();
+
+  //$('.image').on('click', function that ids random photo, sets alert
+}
+
+function renderPeople() {
+  people.forEach((person) => {
+    //person is the specific name for the individual indices of the people array
+    // looping through 'people' array, 'append' jQuery/html into targeted div with .peopleRows class
+    console.log("in renderPeople");
+    $(".peopleRows").append(`
+            <div onClick="">
+                ${person.userProfile}             
+            </div>
+        `);
+  });
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function nameRandomizor() {
+  const nameIndex = randomNumber(0, people.length - 1);
+  //take a number from the array limited to the length of array targeting the name property
+  const randomName = people[nameIndex].name;
+
+  $(".randomNameDiv").append(`
+        <div>
+            ${randomName}             
+        </div>`);
+}
+
+console.log("Here are all the available people:", people);
--- data.js
+++ data.js
@@ -1,30 +1,39 @@
 var people = [
   {
     name: 'Dev',
-    githubUsername: 'devjanaprime'
+    githubUsername: 'devjanaprime',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/17906158?size=250" alt="Profile image of Dev">`
+
   },
   {
     name: 'Dane',
-    githubUsername: 'DoctorHowser'
+    githubUsername: 'DoctorHowser',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/11574995?size=250" alt="Profile image of Dane">`
   },
   {
     name: 'Matt',
-    githubUsername: 'matthew-black'
+    githubUsername: 'matthew-black',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/5817534?size=250" alt="Profile image of Matt">`
   },
   {
     name: 'Kris',
-    githubUsername: 'kdszafranski'
+    githubUsername: 'kdszafranski',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/5541481?size=250" alt="Profile image of Kris">`
   },
   {
     name: 'Edan',
-    githubUsername: 'eschwartz'
+    githubUsername: 'eschwartz',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/1153371?size=250" alt="Profile image of Edan">`
   },
   {
     name: 'Liz',
-    githubUsername: 'emkerber'
+    githubUsername: 'emkerber',
+    userProfile: `<img src="https://avatars.githubusercontent.com/u/17734101?size=250" alt="Profile image of Liz">`
   },
   {
     name: 'Chris',
-    githubUsername: 'christopher-black'
+    githubUsername: 'christopher-black',
+    userProfile: `<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">`
+    
   },
 ];
